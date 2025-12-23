942cd9fcf3332f7eb5d6a6be422f9ba81baa6ed0
Daniel Lozano <danyfriko@gmail.com>
2025-12-23 17:17:49 -0500

    style(login): responsive en layout

--- plataforma-epa-front/src/layouts/components/Footer.jsx
+++ plataforma-epa-front/src/layouts/components/Footer.jsx
@@ -2,12 +2,14 @@ const currentYear = new Date().getFullYear();
 
 export const Footer = () => {
   return (
-    <footer className="bg-epaColor1 text-white flex justify-between items-center p-4">
-      <div>© {currentYear} Empresas Publicas de Armenia E.S.P.</div>
-      <div className="font-bold">Plataforma EPA</div>
-      <div>
-        Contacto de Soporte:{' '}
-        <a href="mailto:redes.tic@epa.gov.co">redes.tic@epa.gov.co</a>
+    <footer className="bg-epaColor1 flex flex-col gap-0.5 px-4 py-2 text-white text-xs sm:flex-row sm:items-center sm:text-base">
+      <div className="sm:flex-1 sm:text-left">© {currentYear} Empresas Publicas de Armenia E.S.P.</div>
+      <div className="text-sm font-bold sm:flex-1 sm:text-2xl sm:text-center">Plataforma EPA</div>
+      <div className="sm:flex-1 sm:text-right">
+        <p>
+          Contacto de Soporte:{' '}
+          <a href="mailto:redes.tic@epa.gov.co">redes.tic@epa.gov.co</a>
+        </p>
         <p>Tel: (606) 741 17 80 Ext. 1512 - 1513</p>
       </div>
     </footer>
